c8d82a669b10646e2277abf1cdfff44259fa8753
Antal Nemes <antal.nemes.hu@gmail.com>
2020-03-03 09:03:10 +0100

    multi_extend_table

--- snippets/stateless_hashtable.py
+++ snippets/stateless_hashtable.py
@@ -21,3 +21,22 @@ assert t("2") == "b"
 import pytest
 with pytest.raises(Exception):
     t("none")
+
+def multi_extend_table(table, *args):
+    if not args:
+        return table
+
+    key = args[0]
+    value = args[1]
+    rest = args[2:]
+
+    return extend_table(multi_extend_table(table, *rest), key, value)
+
+t2 = multi_extend_table(new_table(), "1", "a", "2", "b", "3", "c")
+assert t2("1") == "a"
+assert t2("2") == "b"
+assert t2("3") == "c"
+
+import pytest
+with pytest.raises(Exception):
+    t2("none")
